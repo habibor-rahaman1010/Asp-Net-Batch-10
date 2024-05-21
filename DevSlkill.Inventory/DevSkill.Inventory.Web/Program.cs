4d5a513e6b7940765450b1137a9dfd557678e70e
habibor-rahaman1010 <habibor144369@gmail.com>
2024-05-21 15:17:19 +0600

    Adminlte theme integration and this theme made my life hell

--- DevSlkill.Inventory/DevSkill.Inventory.Web/Program.cs
+++ DevSlkill.Inventory/DevSkill.Inventory.Web/Program.cs
@@ -83,8 +83,14 @@ namespace DevSkill.Inventory.Web
                 app.UseAuthorization();
 
                 app.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                    );
+
+                app.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
                 app.MapRazorPages();
 
                 app.Run();
