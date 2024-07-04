7787ca7972da6835302864d0c4c6908232c6b567
habibor-rahaman1010 <habibor144369@gmail.com>
2024-07-04 11:52:16 +0600

    Add product entity and repository pattern

---
+++ DevSlkill.Inventory/DevSkill.Inventory.Domain/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class Category : IEntity<Guid>
+    {
+        public Guid Entity { get; set; }
+        public string CategoryName{ get; set; } = string.Empty;
+    }
+}
---
+++ DevSlkill.Inventory/DevSkill.Inventory.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkill.Inventory.Domain.Entities
+{
+    public class Product : IEntity<Guid>
+    {
+        public Guid Entity { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty ;
+        public decimal Price { get; set; }
+        public decimal Ratings { get; set; }
+        public Category ProductCategory { get; set; }
+
+        public Product() 
+        {
+            ProductCategory = new Category();
+        }
+    }
+}
