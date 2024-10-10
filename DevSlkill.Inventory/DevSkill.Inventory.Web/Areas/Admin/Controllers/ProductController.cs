5da4a754dd58e8dd8898067b5851170b84daf0f5
habibor-rahaman1010 <habibor144369@gmail.com>
2024-10-10 22:17:56 +0600

    ok

--- DevSlkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/ProductController.cs
+++ DevSlkill.Inventory/DevSkill.Inventory.Web/Areas/Admin/Controllers/ProductController.cs
@@ -120,7 +120,7 @@ namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
             return View(model);
         }
 
-        //use stored procedure
+        //Here used stored procedure for data retrive
         [Route("/Admin/Product/GetProductJsonDataSpAsync")]
         [HttpPost]
         public async Task<JsonResult> GetProductJsonDataSpAsync([FromBody] ProductListModel model)
@@ -173,19 +173,6 @@ namespace DevSkill.Inventory.Web.Areas.Admin.Controllers
             return View(model);
         }
 
-
-      /*  private async Task<string> UploadImage(string folderPath, IFormFile file)
-        {
-
-            folderPath += Guid.NewGuid().ToString() + "_" + file.FileName;
-
-            string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folderPath);
-
-            await file.CopyToAsync(new FileStream(serverFolder, FileMode.Create));
-
-            return "/" + folderPath;
-        }
-      */
 
         [HttpPost, ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProductCreateModel model)
