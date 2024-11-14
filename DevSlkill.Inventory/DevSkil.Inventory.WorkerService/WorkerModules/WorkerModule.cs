cb2e805e5cadb00b7b15fbc5c0a34219a6831884
habibor-rahaman1010 <habibor144369@gmail.com>
2024-11-14 13:02:15 +0600

    Adding worker service feature into my project

---
+++ DevSlkill.Inventory/DevSkil.Inventory.WorkerService/WorkerModules/WorkerModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSkil.Inventory.WorkerService.WorkerModules
+{
+    public class WorkerModule : Module
+    {
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public WorkerModule(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+    }
+}
